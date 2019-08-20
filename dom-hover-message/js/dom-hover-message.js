595ce33c71b43ed45c625d7a8d4685cc507dfc5b
gesila <370225203@qq.com>
2019-08-20 23:35:59 +0800

    add message box style, add create messbox element js code

--- dom-hover-message/js/dom-hover-message.js
+++ dom-hover-message/js/dom-hover-message.js
@@ -3,11 +3,15 @@
 */
 class HoverMessage {
   constructor(domParent, data) {
+    //  父级容器
     this.domParent = domParent;
+    //  存储数据信息
     this.data = data;
-    this.messageBox = null;
+    this.boxContain = null;
+    this.boxContent = null;
     this.handleWatchParam();
     this.initMouseEvent();
+    this.createMessageBox();
   }
 
   initMouseEvent() {
@@ -15,7 +19,7 @@ class HoverMessage {
     that.domParent.onmouseover = function(e) {
       let index = e.target.dataset.index;
       if (index) {
-        console.log('onmouseover', index);
+        console.log("onmouseover", index);
         // that.watchParam.currentIndex = index;
       }
     };
@@ -25,20 +29,15 @@ class HoverMessage {
         let toIndex = e.toElement.dataset.index;
         //  鼠标离开指定位置
         if (fromIndex && !toIndex) {
-          console.log('onmouseout');
+          console.log("onmouseout");
           // that.watchParam.currentIndex = -1;
         }
       } catch (err) {
         //  属性读取失败，符合鼠标离开条件
-        console.log('onmouseout');
+        console.log("onmouseout");
         // that.watchParam.currentIndex = -1;
       }
     };
-  }
-
-  destoryMouseEvent() {
-    this.domParent.onmouseover = null;
-    this.domParent.onmouseout = null;
   }
 
   //  检测 currentIndex 是否变化
@@ -47,10 +46,10 @@ class HoverMessage {
       {},
       {
         set: function(obj, prop, value) {
-          if (prop === 'currentIndex') {
-            console.log('same', obj[prop] === value);
+          if (prop === "currentIndex") {
+            console.log("same", obj[prop] === value);
             obj[prop] = value;
-            console.log('set ' + obj[prop]);
+            console.log("set " + obj[prop]);
           }
           return true;
         }
@@ -59,8 +58,27 @@ class HoverMessage {
   }
 
   // 创建消息弹窗
-  createMessageBox() {}
+  createMessageBox() {
+    this.boxContain = document.createElement("DIV");
+    this.boxContain.classList = "message-box message-box-top";
+    this.boxContent = this.createMessageBoxContent();
+    this.boxContain.appendChild(this.boxContent);
+    document.body.appendChild(this.boxContain);
+  }
 
   // 创建消息弹窗
-  createMessageBoxBody() {}
+  createMessageBoxContent() {
+    let content = "<p>标题</p>";
+    let boxContent = document.createElement("DIV");
+    return boxContent;
+  }
+
+  //
+
+  //  销毁弹窗及监听事件
+  destoryMouseEvent() {
+    document.body.removeChild(this.boxContain);
+    this.domParent.onmouseover = null;
+    this.domParent.onmouseout = null;
+  }
 }
